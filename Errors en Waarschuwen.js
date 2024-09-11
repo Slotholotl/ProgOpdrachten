da576c0b540d184fcfd1da5bdd187f839cd2ca9e
Sløth <remisoffner@gmail.com>
2024-09-11 16:30:37 +0200

    geëxperimenteerd met console.

---
+++ Errors en Waarschuwen.js
@@ -0,0 +1,6 @@
+let s = "Remi";
+    console.log(s);
+let a = "20";
+    console.warn(a);
+let d = "Hugo Keizer"
+    console.error(d)
---
+++ Jam.js
@@ -0,0 +1,4 @@
+let a = "Firetruck";
+    let b = a.slice(0,1);
+        let c = a.slice(6,9);
+            console.log(b+c);
