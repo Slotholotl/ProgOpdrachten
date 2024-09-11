b0a3575484d2fdc9e490d7b6ab419910d97bfa82
Sløth <remisoffner@gmail.com>
2024-09-11 17:03:33 +0200

    Tijd en datum

---
+++ Tijd en Tijdreizen.js
@@ -0,0 +1,12 @@
+
+    const date = new Date(1985, 6, 12, 10, 39, 5, 10);    
+    console.log(date);
+
+    const date2 = new Date(1955, 8, 23, 10,39, 5, 10);    
+    console.log(date2);
+
+    const date3 = new Date(2015, 7, 31, 10, 39, 5, 10);    
+    console.log(date3);
+
+    const date4 = new Date(1885, 0, 4, 10, 39, 5, 10);    
+    console.log(date4);
