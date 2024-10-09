9d2af8a536b9ca670539369508a51dc4152b96e1
Remi <35529@ma-web.nl>
2024-10-09 15:04:02 +0200

    add & loop

---
+++ add.js
@@ -0,0 +1,12 @@
+
+function largerNum(getal5, getal9){
+    if (getal5 > getal9) {
+        return getal5;
+    } else if (getal5 < getal9) {
+        return getal9;
+
+        }    return 0;
+    
+}
+
+console.log (largerNum (5, 1))
---
+++ loops.js
@@ -0,0 +1,4 @@
+for(let i = 1; i < 51; i++){
+    for(let j = 1; j < 51; j++)
+        console.log(i + " x " + j + " = " + (i*j));
+}
